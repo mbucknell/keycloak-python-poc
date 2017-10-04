04be0a61e4936a4984582f0087f80dbf0504c3bf
Mary Bucknell <mbucknell@usgs.gov>
2017-10-04 13:41:23 -0500

    Changes to use new flask_restplus_jwt module to provide authentication/authorization.

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
 
 DEBUG = False
-JWT_ALGORITHM = 'RS256'
+JWT_ALGORITHM = 'HS256'
 JWT_PUBLIC_KEY = os.environ.get('JWT_PUBLIC_KEY', '')
+JWT_SECRET_KEY = 'secret'
 
-JWT_DECODE_AUDIENCE = os.environ.get('JWT_DECODE_AUDIENCE', 'security-admin-console')
+#JWT_DECODE_AUDIENCE = os.environ.get('JWT_DECODE_AUDIENCE', 'security-admin-console')
+
+def get_role (dict):
+    return dict['role']
+JWT_ROLE_CLAIM = get_role
+JWT_ROLE = 'admin'
--- services.py
+++ services.py
@@ -1,10 +1,10 @@
-from flask import request
-from flask_restplus import Api, Resource
-from flask_jwt_simple import JWTManager, jwt_required
-from flask_jwt_simple.exceptions import NoAuthorizationError
 
-from jwt.exceptions import ExpiredSignatureError, DecodeError, InvalidAudienceError
+from flask_restplus import Api, Resource
+from flask_restplus_jwt import JWTRestplusManager, jwt_required, jwt_role_required
+
 from app import application
+
+
 api = Api(application,
           title='Keycloak POC',
           default='Keycloak POC',
@@ -13,23 +13,21 @@ api = Api(application,
 
 
 # Setup the Flask-JWT-Simple extension
-jwt = JWTManager(application)
-
-# Adding all jwt exceptions that we care about to the api. Otherwise they all return a 500.
-# The status returned for each error is determined by it's default error handler
-# If we want to override the default status, we will need to override the default flask-jwt-simple
-# functions using loader decorators
-@api.errorhandler(ExpiredSignatureError) #Status 401
-@api.errorhandler(NoAuthorizationError) #Status 401
-@api.errorhandler(DecodeError) # Returns status 422
-@api.errorhandler(InvalidAudienceError) # Status 422
-def handler_invalid_token(error):
-    return {'message': error.message}
+jwt = JWTRestplusManager(api, application)
 
 
 @api.route('/endpoint')
 class TestEndpoint(Resource):
 
+    @api.header('Authorization', 'JWT token', required=True)
     @jwt_required
     def get(self):
         return "Successful"
+
+@api.route('/role_endpoint')
+class TestRoleProtectedEndPoint(Resource):
+
+    @api.header('Authorization', 'JWT token', required=True)
+    @jwt_role_required('admin')
+    def get(self):
+        return 'Successful'
